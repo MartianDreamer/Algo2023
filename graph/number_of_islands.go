2e133b65513e2682a3323a524164cd6b1d665f48
MartianDreamer <nguyenxuansang9494@gmail.com>
2024-05-28 08:00:27 +0000

    little more optimization

--- graph/number_of_islands.go
+++ graph/number_of_islands.go
@@ -10,39 +10,39 @@ func NumIslands(grid [][]byte) int {
 				continue
 			}
 			count++
-			dfs(ic, ir, row, col, grid)
+			dfs(ic, ir, row, col, &grid)
 		}
 	}
 	return count
 }
 
-func neighbors(islandX int, islandY int, row int, col int, graph [][]byte) [][2]int {
+func neighbors(islandX int, islandY int, row int, col int) [][2]int {
 	rs := make([][2]int, 0, 4)
 	left := islandX - 1
 	right := islandX + 1
 	upper := islandY - 1
 	lower := islandY + 1
-	if left >= 0 && graph[islandY][left] == 49 {
+	if left >= 0 {
 		rs = append(rs, [2]int{left, islandY})
 	}
-	if right < col && graph[islandY][right] == 49 {
+	if right < col {
 		rs = append(rs, [2]int{right, islandY})
 	}
-	if upper >= 0 && graph[upper][islandX] == 49 {
+	if upper >= 0 {
 		rs = append(rs, [2]int{islandX, upper})
 	}
-	if lower < row && graph[lower][islandX] == 49 {
+	if lower < row {
 		rs = append(rs, [2]int{islandX, lower})
 	}
 	return rs
 }
 
-func dfs(islandX int, islandY int, row int, col int, graph [][]byte) {
-	if graph[islandY][islandX] != 49 {
+func dfs(islandX int, islandY int, row int, col int, graph *[][]byte) {
+	if (*graph)[islandY][islandX] != 49 {
 		return
 	}
-	graph[islandY][islandX] = 50
-	neighbors := neighbors(islandX, islandY, row, col, graph)
+	(*graph)[islandY][islandX] = 50
+	neighbors := neighbors(islandX, islandY, row, col)
 	for _, v := range neighbors {
 		dfs(v[0], v[1], row, col, graph)
 	}
