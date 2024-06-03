6407be0c6af31a1148349f23c8e3d7207ca3d713
MartianDreamer <nguyenxuansang9494@gmail.com>
2024-06-03 02:11:52 +0000

    pacific water flow

--- README.md
+++ README.md
@@ -44,7 +44,7 @@ Graph
 
 - [x] Clone Graph - https://leetcode.com/problems/clone-graph/ - [Solution](/graph/133_clone_graph.py)
 - [x] Course Schedule - https://leetcode.com/problems/course-schedule/ - [Solution](/graph/207_course_schedule.py)
-- [x] Pacific Atlantic Water Flow - https://leetcode.com/problems/pacific-atlantic-water-flow/
+- [x] Pacific Atlantic Water Flow - https://leetcode.com/problems/pacific-atlantic-water-flow/ - [Solution](/graph/417_pacific_water_flow.py)
 - [x] Number of Islands - https://leetcode.com/problems/number-of-islands/ - [Solution](/graph/200_number_of_islands.py)
 - [ ] Longest Consecutive Sequence - https://leetcode.com/problems/longest-consecutive-sequence/
 - [ ] Alien Dictionary (Leetcode Premium) - https://leetcode.com/problems/alien-dictionary/
--- graph/417_pacific_water_flow.py
+++ graph/417_pacific_water_flow.py
@@ -3,38 +3,69 @@ from typing import List
 
 class Solution:
     # TODO not done yet
-    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        hSize, vSize = len(heights), len(heights[0])
-        pacificFlow = [[False] * vSize for _ in range(hSize)]
-        if vSize == 1 and hSize == 1:
-            return [[0, 0]]
+    def pacificAtlantic(self, heights):
         rs = []
-        sharedCells = ((0, vSize - 1), (hSize - 1, 0))
+        pacificCells = []
+        atlantisCells = []
+        rowQuan = len(heights)
+        colQuan = len(heights[0])
+        pacificVisitted = [[False] * colQuan for _ in range(rowQuan)]
+        atlantisVisitted = [[False] * colQuan for _ in range(rowQuan)]
         queue = []
-        for c in sharedCells:
-            queue.append(c)
+        for i in range(rowQuan):
+            pacificCells.append([i, 0])
+            atlantisCells.append([i, colQuan - 1])
+        for i in range(colQuan):
+            pacificCells.append([0, i])
+            atlantisCells.append([rowQuan - 1, i])
+
+        # find all the cells can reach Pacific
+        for cell in pacificCells:
+            r, c = cell[0], cell[1]
+            if pacificVisitted[r][c]:
+                continue
+            pacificVisitted[r][c] = True
+            queue.append(cell)
             while len(queue) > 0:
-                qe = queue.pop(0)
-                c, r = qe[0], qe[1]
-                rs.append([r, c])
-                for nb in self.neighbors(c, r, hSize, vSize):
-                    if heights[nb[1]][nb[0]] > heights[r][c]:
-                        queue.append(nb)
-                heights[r][c] = -1
+                head = queue.pop(0)
+                headR, headC = head[0], head[1]
+                for n in self.__neighbors(headR, headC, rowQuan, colQuan):
+                    nR, nC = n[0], n[1]
+                    if not pacificVisitted[nR][nC] and heights[nR][nC] >= heights[headR][headC]:
+                        pacificVisitted[nR][nC] = True
+                        queue.append(n)
+
+        # find all the cells can reach Pacific and Atlantis
+        for cell in atlantisCells:
+            r, c = cell[0], cell[1]
+            if atlantisVisitted[r][c]:
+                continue
+            atlantisVisitted[r][c] = True
+            queue.append(cell)
+            while len(queue) > 0:
+                head = queue.pop(0)
+                headR, headC = head[0], head[1]
+                if pacificVisitted[headR][headC]:
+                    rs.append(head)
+                for n in self.__neighbors(headR, headC, rowQuan, colQuan):
+                    nR, nC = n[0], n[1]
+                    if not atlantisVisitted[nR][nC] and heights[nR][nC] >= heights[headR][headC]:
+                        atlantisVisitted[nR][nC] = True
+                        queue.append(n)
+
         return rs
 
-    def neighbors(self, c: int, r: int, hSize: int, vSize: int):
+    def __neighbors(self, r, c, rowQuan, colQuan):
         rs = []
-        if c - 1 >= 0:
-            rs.append((c - 1, r))
-        if c + 1 < vSize:
-            rs.append((c+1, r))
         if r - 1 >= 0:
-            rs.append((c, r - 1))
-        if r + 1 < hSize:
-            rs.append((c, r + 1))
+            rs.append([r - 1, c])
+        if r + 1 < rowQuan:
+            rs.append([r + 1, c])
+        if c - 1 >= 0:
+            rs.append([r, c - 1])
+        if c + 1 < colQuan:
+            rs.append([r, c + 1])
         return rs
 
 
-print(Solution().pacificAtlantic([[1, 2, 2, 3, 5], [3, 2, 3, 4, 4], [
-      2, 4, 5, 3, 1], [6, 7, 1, 4, 5], [5, 1, 1, 2, 4]]))
+print(Solution().pacificAtlantic([[1,1],[1,1],[1,1]]))
