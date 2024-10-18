1d6ac26bbac8d852e1504f5e4160990bcfefae4a
MartianDreamer <nguyenxuansang9494@gmail.com>
2024-10-18 15:29:11 +0700

    98

--- random/98_validate_bst.py
+++ random/98_validate_bst.py
@@ -22,5 +22,5 @@ def is_valid_bst(root: Optional[TreeNode], upper_bound: int = 2**31, lower_bound
             or (root.left.val < root.val and root.left.val > lower_bound))    \
         and (root.right is None
              or (root.right.val > root.val and root.right.val < upper_bound))   \
-        and is_valid_bst(root.left, min(root.val, upper_bound), lower_bound)    \
-        and is_valid_bst(root.right, upper_bound, max(root.val, lower_bound))
+        and is_valid_bst(root.left, root.val, lower_bound)    \
+        and is_valid_bst(root.right, upper_bound, root.val)
