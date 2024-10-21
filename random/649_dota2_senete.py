088e57f6b6fd6d4f5e43ad3ae0e9b09e49e878b4
MartianDreamer <nguyenxuansang9494@gmail.com>
2024-10-21 09:30:08 +0700

    649

---
+++ random/649_dota2_senete.py
@@ -0,0 +1,27 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        senators = [x for x in senate]
+        while True:
+            for i in range(len(senators)):
+                if senators[i] == "D":
+                    if "R" not in senators:
+                        return "Dire"
+                    else:
+                        try:
+                            next_r = senators.index("R", i + 1)
+                            senators[next_r] = "x"
+                        except:
+                            next_r = senators.index("R")
+                            senators[next_r] = "x"
+                elif senators[i] == "R":
+                    if "D" not in senators:
+                        return "Radiant"
+                    else:
+                        try:
+                            next_d = senators.index("D", i + 1)
+                            senators[next_d] = "x"
+                        except:
+                            next_d = senators.index("D")
+                            senators[next_d] = "x"
+
+print(Solution().predictPartyVictory("DRRDRDRDRDDRDRDR"))
