f57daf029fa78e31129dfd83ef56aa42273fdfa5
MartianDreamer <nguyenxuansang9494@gmail.com>
2024-10-22 23:55:08 +0700

    1372

--- tree/1372_longest_zigzag_tree.py
+++ tree/1372_longest_zigzag_tree.py
@@ -6,19 +6,23 @@ from tree_impl import TreeNode
 
 class Solution:
     def longestZigZag(self, root: Optional[TreeNode]) -> int:
-        LEFT_OF_PARENT, RIGHT_OF_PARENT = 0, 1
         max_val = 0
-        def d(root: Optional[TreeNode], prev_count: int,parent_side: int):
+        def dfs_left(root: Optional[TreeNode], prev_count: int):
             if root is None:
                 nonlocal max_val
                 max_val = max(max_val, prev_count)
                 return
-            if parent_side == LEFT_OF_PARENT:
-                d(root.right, 1 + prev_count, RIGHT_OF_PARENT)
-                d(root.left, 0, LEFT_OF_PARENT)
-            else:
-                d(root.left, 1 + prev_count, LEFT_OF_PARENT)
-                d(root.right, 0, RIGHT_OF_PARENT)
-        d(root.left, 0, LEFT_OF_PARENT)
-        d(root.right, 0, RIGHT_OF_PARENT)
+            dfs_right(root.right, 1 + prev_count)
+            dfs_left(root.left, 0)
+        
+        def dfs_right(root: Optional[TreeNode], prev_count: int):
+            if root is None:
+                nonlocal max_val
+                max_val = max(max_val, prev_count)
+                return
+            dfs_left(root.left, 1 + prev_count)
+            dfs_right(root.right, 0)
+
+        dfs_left(root.left, 0)
+        dfs_right(root.right, 0)
         return max_val
