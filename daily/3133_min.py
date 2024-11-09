b27556029894194813f8daedea11bf7d7ec35eb1
MartianDreamer <nguyenxuansang9494@gmail.com>
2024-11-09 15:01:30 +0700

    3133

--- daily/3133_min.py
+++ daily/3133_min.py
@@ -1,39 +1,25 @@
-from functools import reduce
-from typing import List
-
 class Solution:
     def minEnd(self, n: int, x: int) -> int:
-        x_bits = []
-        while x > 1:
-            x_bits.append(x & 1)
-            x >>= 1
-        x_bits.append(x)
+        n -= 1
+        c = 0
 
-        n = n - 1
-        cur_bit = 0
+        # min end for x = 0b100110, n - 1 = 15 (0b1111) is 0b1111111
+        # go through every bit of n - 1
+        # or the bit with zero bit in x
+        # consider there are zeros before the most significant bit of x
+
         while n > 0:
-            least_significant_bit = n & 1
+            lsb = n & 1
             n >>= 1
 
-            i = cur_bit
-            should_append = True
-            for i in range(cur_bit, len(x_bits)):
-                if x_bits[i] == 0:
-                    cur_bit = i
-                    x_bits[cur_bit] = least_significant_bit
-                    should_append = False
-                    break
 
-            if should_append:
-                cur_bit = len(x_bits)
-                x_bits.append(least_significant_bit)
-            
-            cur_bit += 1
+            remember_x = x
+            x >>= c
+            while x & 1:
+                c += 1
+                x >>= 1
+            x = (x | lsb) << c | remember_x
+            c += 1
 
-        rs = 0
-        for i in range(len(x_bits)):
-            rs = rs | x_bits[i] << i
 
-        return rs
-
-print(Solution().minEnd(7, 2))
+        return x
