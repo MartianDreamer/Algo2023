a320c4a7ac9337e443268d085edbbea70b38a87b
MartianDreamer <nguyenxuansang9494@gmail.com>
2024-11-16 01:11:48 +0700

    637

--- tree/637_average.py
+++ tree/637_average.py
@@ -7,26 +7,24 @@ class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
         if root is None:
             return []
-        
+
         border = [root]
         bcount = 1
         rs: List[float] = [root.val]
+        total = 0
 
-        def bfs():
-            nonlocal bcount
-            while len(border) > 0:
-                if bcount == 0:
-                    bcount = len(border)
-                    rs.append(sum([b.val for b in border])/bcount)
-                frontier = border.pop(0)
-                bcount -= 1
-                if frontier.left is not None:
-                    border.append(frontier.left)
-                if frontier.right is not None:
-                    border.append(frontier.right)
+        while len(border) > 0:
+            if bcount == 0:
+                bcount = len(border)
+                rs.append(total/bcount)
+                total = 0
+            frontier = border.pop(0)
+            bcount -= 1
+            if frontier.left is not None:
+                border.append(frontier.left)
+                total += frontier.left.val
+            if frontier.right is not None:
+                border.append(frontier.right)
+                total += frontier.right.val
 
-        bfs()
         return rs
-
-t = TreeNode.make_tree([3,9,20,30,35,15,7])
-print(Solution().averageOfLevels(t))
