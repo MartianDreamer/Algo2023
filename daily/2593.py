705c525a404cac6ee191db24ab43f0395f32ca29
MartianDreamer <nguyenxuansang9494@gmail.com>
2024-12-13 14:46:13 +0700

    2593

--- daily/2593.py
+++ daily/2593.py
@@ -2,16 +2,14 @@ from typing import List
 
 class Solution:
     def findScore(self, nums: List[int]) -> int:
-        sorted_nums = sorted([(n, i) for i, n in enumerate(nums)], key=lambda e: e[0])
+        sorted_indices = sorted([i for i in range(len(nums))], key=lambda e: nums[e])
         rs = 0
-        for sn, si in sorted_nums:
-            if nums[si] == 1_000_001:
+        for i in sorted_indices:
+            if nums[i] == 1_000_001:
                 continue
-            rs += sn
-            if si > 0 and nums[si - 1] != 1_000_001:
-                nums[si - 1] = 1_000_001
-            if si < len(nums) - 1 and nums[si + 1] != 1_000_001:
-                nums[si + 1] = 1_000_001
+            rs += nums[i]
+            if i > 0 and nums[i - 1] != 1_000_001:
+                nums[i - 1] = 1_000_001
+            if i < len(nums) - 1 and nums[i + 1] != 1_000_001:
+                nums[i + 1] = 1_000_001
         return rs
-
-print(Solution().findScore([2, 1, 3, 4, 5, 2]))
